ae4b5169cde754b5957910b3912d9880be066442
Miguel Angel González Hernández <migueljabugo@gmail.com>
2017-11-30 20:32:05 +0100

    no message

---
+++ Space invaders/Assets/scripts/Bullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    //DECLARAMOS LAS VARIABLES PARA LA BALA QUE SALE DEL PLAYER.
+    private Transform bullet;
+    public float speed;
+	public float limite = 5;
+	bool fire = false;
+	
+	void Start () {
+        //USAMOS EL COMPONENTE TRANSFORM DE LA BALA.
+        bullet = GetComponent<Transform>();
+	}
+	
+	
+	void FixedUpdate () {
+        //AÑADIMOS LA DIRECCION A LA QUE SE MOVERA LA BALA, 
+        //USANDO LA VELOCIDAD QUE AÑADIMOS EN EL FLOAT speed.
+		if (Input.GetButton("Fire")) {
+			fire = true;
+		}
+		if (fire) {
+			bullet.position += Vector3.up * speed;
+			if (bullet.position.y >= limite) {
+				Destroy (gameObject);
+			}
+		}
+    
+     
+	}
+
+    //private void OnTriggerEnter2D(Collider2D enemy)
+    //{
+    //    if (enemy.tag == "Enemy") 
+    //    {
+    //        Destroy(enemy.gameObject);
+    //        Destroy(gameObject);
+    //    }
+    //    else if (enemy.tag == "boss")
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+}
---
+++ Space invaders/Assets/scripts/generateInvaders.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateInvaders : MonoBehaviour {
+
+	public GameObject invader;
+	public float separationVertical=1;
+	public float separationHorizontal=1;
+	public int rows = 5;
+	public int colums=11;
+
+
+	void Start () {
+		//Recoger la posición inicial de los invaders
+		float posVert = transform.position.y;
+		float posHor = transform.position.x;
+		for (int y = 0; y < rows; y++) {
+			//Coloca columna y
+			for (int x = 0; x < colums; x++) {
+				//Colocando fila x
+				GameObject cube = GameObject.Instantiate (invader); 
+				cube.transform.parent = this.gameObject.transform;
+				cube.transform.position = new Vector2 (posHor + x, posVert - y);
+				posHor += separationHorizontal;
+			}
+			posHor = transform.position.x;
+			posVert -= separationVertical;
+		}
+	}
+}
